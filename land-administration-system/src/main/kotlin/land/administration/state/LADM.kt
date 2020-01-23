6381367a43c5559abee637a4b40a376d380e0842
andelm <Marc.vanAndel@kadaster.nl>
2020-01-23 07:52:34 +0100

    RRR impl

--- land-administration-system/src/main/kotlin/land/administration/state/LADM.kt
+++ land-administration-system/src/main/kotlin/land/administration/state/LADM.kt
@@ -5,13 +5,46 @@ import java.time.LocalDateTime
 data class ObjectIdentifier(val namespace: String, val localId: String)
 
 data class Fraction(val denominator: Int, val numerator: Int) {
-    fun real() = denominator/numerator
+    fun real() = denominator / numerator
 }
 
 abstract class RightRestrictionOrResponsibility(
-    val description: String,
-    val rId: ObjectIdentifier,
-    val share: Fraction,
-    val shareCheck: Boolean,
-    val timeSpec: LocalDateTime
+        open val description: String,
+        open val rId: ObjectIdentifier,
+        open val share: Fraction,
+        open val shareCheck: Boolean,
+        open val timeSpec: LocalDateTime
 )
+
+data class Responsibility(
+        override val description: String,
+        override val rId: ObjectIdentifier,
+        override val share: Fraction,
+        override val shareCheck: Boolean,
+        override val timeSpec: LocalDateTime
+) : RightRestrictionOrResponsibility(description, rId, share, shareCheck, timeSpec)
+
+data class Restriction(
+        override val description: String,
+        override val rId: ObjectIdentifier,
+        override val share: Fraction,
+        override val shareCheck: Boolean,
+        override val timeSpec: LocalDateTime
+) : RightRestrictionOrResponsibility(description, rId, share, shareCheck, timeSpec)
+
+data class Right(
+        override val description: String,
+        override val rId: ObjectIdentifier,
+        override val share: Fraction,
+        override val shareCheck: Boolean,
+        override val timeSpec: LocalDateTime
+) : RightRestrictionOrResponsibility(description, rId, share, shareCheck, timeSpec)
+
+data class BasicAdministrativeUnit(
+        val name: String,
+        val type: BasicAdministrativeUnitType,
+        val uid: ObjectIdentifier)
+
+enum class BasicAdministrativeUnitType {
+
+}
